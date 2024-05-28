5dca0b3c91a0d7bb1a844e185b5ecf5d39a57cd9
jgravalo <jesusgrawork361@gmail.com>
2024-05-29 00:44:27 +0200

    env_var added

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jgravalo <jgravalo@student.42barcel>       +#+  +:+       +#+        */
+/*   By: jgravalo <jgravalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 17:09:13 by jgravalo          #+#    #+#             */
-/*   Updated: 2022/07/14 15:35:26 by jgravalo         ###   ########.fr       */
+/*   Updated: 2024/05/29 00:43:55 by jgravalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1
+#endif
 
 # include <stdio.h>
 # include <fcntl.h>
